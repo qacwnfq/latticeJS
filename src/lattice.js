84a62a57309d07a1e7a99d8f797e90f89b439a0d
Fredrik <fredrik.jadebeck@gmail.com>
2017-08-30 17:04:48 +0200

    Add calculation of array length.

--- src/lattice.js
+++ src/lattice.js
@@ -1,8 +1,17 @@
 'use strict';
 
-const point = require( './point' );
+module.exports = function lattice( params, options={} ) {
+   const {
+      dims,
+      lattice
+   } = params;
+   const { periodic } = options;
+   this.dims = dims;
+   this.dim = dims.length;
+   this.lattice = lattice ? lattice: new Array( 
+      this.dims.reduce( ( a, b ) => a*b )
+   );
+   this.periodic = periodic ? true : false;
 
-module.exports = function( dim ) {
-   this.dim = dim;
 }
 
--- src/point.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-exports.point = function point( value, coords ){
-   this.dim = coords.length; 
-   this.coords = coords;
-   this.value = value;
-}
