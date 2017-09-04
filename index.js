b686205c85abc34e679c655ef073744a6aa1844d
Fredrik <fredrik.jadebeck@gmail.com>
2017-09-04 20:06:43 +0200

    WIP pythonic _repr_ to debug.

--- index.js
+++ index.js
@@ -3,11 +3,7 @@
 const lattice = require( './src/lattice' );
 
 //module.exports.lattice = lattice;
-const l = new lattice( { dims: [ 3, 2 ] }, { periodic: true } );
-//l.set( 1, [ 0, 1 ] );
-console.log( l.point( [ 0, 0 ] ) );
-console.log( l.point( [ 0, 1 ] ) );
-console.log( l.point( [ 1, 0 ] ) );
-console.log( l.point( [ 1, 1 ] ) );
-console.log( l.point( [ 2, 0 ] ) );
-console.log( l.point( [ 2, 1 ] ) );
+const l = new lattice( { dims: [ 3, 4 ] }, { periodic: true } );
+l.set( 1, 2 );
+console.log( l.point( 2 ) );
+console.log( l );
--- src/lattice.js
+++ src/lattice.js
@@ -1,6 +1,8 @@
 'use strict';
 
-module.exports = function lattice( params, options={} ) {
+module.exports = lattice;
+
+function lattice( params, options={} ) {
    const {
       dims,
       lattice
@@ -12,11 +14,31 @@ module.exports = function lattice( params, options={} ) {
       this.dims.reduce( ( a, b ) => a*b )
    ).fill( 0 );
    this.periodic = periodic ? true : false;
-   this.point = function( ...coords ) {
-      return this.lattice[ coords % this.dim[ coords ] ];
-   };
-   this.set = function( value, ...coords ) {
-      this.lattice[ coords % this.dim[ coords ] ] = value;
-   }
+   this.point = point.bind( this );
+   this.set = set.bind( this );
+
 }
 
+lattice.prototype.inspect = function() {
+   let result = '';
+   // TODO if this remains, replace with map, reduce combo
+   this.dims.forEach( dim => {
+      Array.apply( null, Array( dim ) ).map( (_, i ) => { return i; } ).forEach(
+         i =>  {
+            result += String( this.lattice[ i ] );
+            result += ' ';
+         } );
+      result +=  '\n';
+   } )
+   return result;
+};
+
+function set( value, ...coords ) {
+   this.lattice[ coords % this.dim[ coords ] ] = Number( value );
+   console.log( this.lattice );
+   console.log( "9999" );
+}
+
+function point( ...coords ) {
+   return this.lattice[ coords % this.dim[ coords ] ];
+}
